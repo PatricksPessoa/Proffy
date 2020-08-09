1230c2c8b0685c08a2b86609ffea61c68c16bf5e
patrickspessoa <patrickspessoa@gmail.com>
2020-08-09 16:27:07 -0300

    all done by vscode - git bash

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+//Dados
+const proffys = [
+    { 
+        name: 'Luiz Gustavo Muller',
+        avatar:'https://scontent.fria3-1.fna.fbcdn.net/v/t1.0-9/11137114_974947785872472_4138925663123034127_n.jpg?_nc_cat=103&_nc_sid=7aed08&_nc_ohc=wYwJvZdfATgAX9xBo6-&_nc_ht=scontent.fria3-1.fna&oh=3340545fa6fa04996bba6eba44a4ffbc&oe=5F517DA6',
+        whatsapp: '899886622',
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "89987654534", 
+        bio: "As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido.<br><br>Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.", 
+        subject: "Geografia", 
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Mayk Brito", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "89987654534", 
+        bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.<br><br>Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: Aprenda a fazer dinheiro com isso!", 
+        subject: "Educação Física", 
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,27 @@
+
+//Servidor
+const express = require('express')
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+//Configurar nunjucks (Template Engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+//Início e configuração do servidor
+server
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//Start do Servidor
+.listen(5000)
